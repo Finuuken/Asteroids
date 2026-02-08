1b1361ea445a4c7e067bbe8add2a831c408ec2aa
Conor <cfinucane45@googlemail.com>
2026-02-08 22:30:21 +0000

    added fps into function

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    pygame.surface.fill(black)
-    display.flip()
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
